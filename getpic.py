70003b72bb002c0db28ced947bee954868c64e5c
PengliangCao <caopengliang@smart-haier.com>
2015-11-13 14:17:18 +0800

    add a method

--- getpic.py
+++ getpic.py
@@ -1,26 +1,32 @@
 # coding=utf-8
-#<a href="http://travel.163.com/food/#f=endnav">
 import urllib.request
 import re
-url = 'http://tieba.baidu.com/p/2460150866'
+#url = 'http://tieba.baidu.com/p/2460150866'
+url='http://image.baidu.com/search/index?tn=baiduimage&ps=1&ct=201326592&lm=-1&cl=2&nc=1&ie=utf-8&word=%E7%99%BE%E5%BA%A6'
 filename='C:\\projects\\PythonTest\\pic\\'
-reg = r'src="(.+?\.jpg)" pic_ext'
+#reg = r'src="(.+?\.jpg)" pic_ext'
+reg = r'src="(.+\.jpg)"'
 data = urllib.request.urlopen(url).read()
 data=data.decode('utf-8')
 r1 = re.compile(reg)
 c_t = r1.findall(data)
 print("pic len：",len(c_t))
 
-for num in list(range(len(c_t))):
-    fn=filename+str(num)+'.jpg'
-    
-    conn = urllib.request.urlopen(c_t[num]) 
-    f = open(fn,'wb')  
+def downloadimage(url,address):
+    conn = urllib.request.urlopen(url) 
+    f = open(address,'wb')  
     f.write(conn.read())  
-    f.close() 
+    f.close()
+
+for num in list(range(len(c_t))):
+    fn=filename+str(num)+'.jpg' 
+    downloadimage(c_t[num],fn)
     print('saved',num+1)	
     num=num+1
+#f.close()
+
 print('Pic Saved!') 
-	
+
+
 #for name in c_t:
 #    print(name)
